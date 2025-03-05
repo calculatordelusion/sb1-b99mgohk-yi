6d7a511b382c3a57f1377054cf1f87c56c196919
Martin Jack <134946937+calculatordelusion@users.noreply.github.com>
2025-03-05 16:19:22 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import { HelmetProvider } from 'react-helmet-async';
+
+function App() {
+  return (
+    <HelmetProvider>
+      {/* Your app components */}
+    </HelmetProvider>
+  );
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
